cd69b81afc62ff0e6ec6c5aabf6b0da485b63a3e
fgcurti <46764922+fgcurti@users.noreply.github.com>
2020-04-23 23:50:40 -0500

    Add basic exception handling

--- src/information.cpp
+++ src/information.cpp
@@ -7,10 +7,20 @@ Information::Information()
 }
 
 // Calculates Area of square shaped county
-double Information::calcArea(double width, double height){
-    return width * height;
+double Information::calcArea(double height, double width){
+    if (width < 0 || height < 0)
+    {
+        throw "Error: Length or height is negative.\n";
+    }
+    else
+        return height * width;
 }
 // Calculates Perimeter of square shaped county
-double Information::calcPerimeter(double width, double height){
-    return 2 * width + 2 * height;
+double Information::calcPerimeter(double height, double width){
+    if (width < 0 || height < 0)
+    {
+        throw "Error: Length or height is negative.\n";
+    }
+    else
+        return 2 * height + 2 * width;
 }
